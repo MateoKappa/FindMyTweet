4d22cb510bbad56a57285764f10e58320fe7c9d4
MateoKappa <kapllanimateo01@gmail.com>
2022-12-27 17:35:17 +0200

    Add new feature

--- findthetweet/pages/index.jsx
+++ findthetweet/pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={styles.main}>
       <LeftNav />
       <Tweets />
-      Yeah
+      Yeah o alekos gamietai
       <Map long={22.99299} lang={39.357669} />
     </div>
   );
